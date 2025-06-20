24d8d271f63f29d252374ab86642ac432eb231e2
@hexlet/cli <support@hexlet.io>
2025-06-20 17:09:05 +0300

    submit java-spring-boot-ru/dto

---
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,66 @@
+package exercise.controller;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+
+// BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping("")
+    public List<PostDTO> index() {
+        return postRepository.findAll().stream()
+                .map(this::convertToPostDTO)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public PostDTO show(@PathVariable long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+
+        return convertToPostDTO(post);
+    }
+
+    private PostDTO convertToPostDTO(Post post) {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setId(post.getId());
+        postDTO.setTitle(post.getTitle());
+        postDTO.setBody(post.getBody());
+
+        List<CommentDTO> commentDTOs = commentRepository.findByPostId(post.getId()).stream()
+                .map(this::convertToCommentDTO)
+                .collect(Collectors.toList());
+
+        postDTO.setComments(commentDTOs);
+        return postDTO;
+    }
+
+    private CommentDTO convertToCommentDTO(Comment comment) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setBody(comment.getBody());
+        return commentDTO;
+    }
+}
+// END
